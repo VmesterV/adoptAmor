5908004535993317f1d375b6e679178654715c42
VmesterV <76802635@continental.edu.pe>
2025-10-20 14:39:15 -0500

    Add multiple script imports for mascotas and tienda

--- vistas/includes/script.php
+++ vistas/includes/script.php
@@ -1,3 +1,12 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="vistas/js/script.js"></script> 
-<script src="vistas/js/script2.js"></script>
+
+<script src="vistas/js/agregar_mascotas/mascotas.js"></script>
+<script src="vistas/js/agregar_mascotas/utiles.js"></script>
+<script src="vistas/js/agregar_mascotas/navegacion.js"></script>
+<script src="vistas/js/agregar_mascotas/main.js"></script>
+
+
+<script src="vistas/js/tienda_objetos/productos.js"></script>
+<script src="vistas/js/tienda_objetos/tienda_main.js"></script>
+<script src="vistas/js/tienda_objetos/funciones.js"></script>
+<script src="vistas/js/tienda_objetos/carrito.js"></script>
