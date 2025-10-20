4ebb0b5159f854bf101c29cf7f94e67369b19798
AnthonyTPL2006 <75429928@continental.edu.pe>
2025-10-20 15:53:16 -0500

    Restore pet data structure in mascotas.js
    eliminar caracteres especiales

--- vistas/js/agregar_mascotas/mascotas.js
+++ vistas/js/agregar_mascotas/mascotas.js
@@ -10,10 +10,11 @@ const perros = [
 ];
 
 const gatos = [
-    { id: "007", nombre: "Simón", raza: "Siamés", edad: "1 años", imagen: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+    { id: "007", nombre: "Simon", raza: "Siames", edad: "1 año", imagen: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
     { id: "008", nombre: "Simba", raza: "Esfinge", edad: "5 años", imagen: "https://i.pinimg.com/736x/ef/8c/dc/ef8cdc3899137eb61f03547419b0518d.jpg" },
-    { id: "009", nombre: "Sasha", raza: "Angora", edad: "3 año", imagen: "https://i.pinimg.com/originals/8e/d7/41/8ed7410285f101ba5892ff723c91fa75.jpg" },
-    { id: "010", nombre: "Peluza", raza: "Persa", edad: "5 año", imagen: "https://www.zooplus.es/magazine/wp-content/uploads/2017/10/fotolia_103481419.webp" },
-    { id: "011", nombre: "Nala", raza: "Bengala", edad: "6 año", imagen: "https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-01/Bengal.1.jpg?itok=98LQ1RfI" },
-    { id: "012", nombre: "Chispa", raza: "Bizco", edad: "4 año", imagen: "https://st.depositphotos.com/2836705/3618/i/450/depositphotos_36180101-stock-photo-cross-eyed-cat.jpg" }
+    { id: "009", nombre: "Sasha", raza: "Angora", edad: "3 años", imagen: "https://i.pinimg.com/originals/8e/d7/41/8ed7410285f101ba5892ff723c91fa75.jpg" },
+    { id: "010", nombre: "Peluza", raza: "Persa", edad: "5 años", imagen: "https://www.zooplus.es/magazine/wp-content/uploads/2017/10/fotolia_103481419.webp" },
+    { id: "011", nombre: "Nala", raza: "Bengala", edad: "6 años", imagen: "https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-01/Bengal.1.jpg?itok=98LQ1RfI" },
+    { id: "012", nombre: "Chispa", raza: "Bizco", edad: "4 años", imagen: "https://st.depositphotos.com/2836705/3618/i/450/depositphotos_36180101-stock-photo-cross-eyed-cat.jpg" }
+
 ];
