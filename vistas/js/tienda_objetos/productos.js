6746fc319721c30da55425b67efbd2c91c49df1a
AnthonyTPL2006 <75429928@continental.edu.pe>
2025-10-20 15:42:17 -0500

    productos.js

--- vistas/js/tienda_objetos/productos.js
+++ vistas/js/tienda_objetos/productos.js
@@ -6,9 +6,10 @@ const productos = [
   { id: 3, nombre: "Cama para mascotas", descripcion: "Cama acolchada de algodón suave, perfecta para descansar.", precio: 80.00, imagen: "https://rapipet.pe/wp-content/uploads/2022/01/Diseno-sin-titulo-3-3.png", stock: 10 },
   { id: 4, nombre: "Comedero doble", descripcion: "Dos compartimientos para agua y comida de acero inoxidable.", precio: 35.90, imagen: "https://img.ltwebstatic.com/images3_spmp/2024/04/25/39/1714006615c136be308d7db1fdc342a17c695da3f5.webp", stock: 10 },
   { id: 5, nombre: "Rascador para gato", descripcion: "Rascador de sisal con base estable y juguete incluido.", precio: 60.00, imagen:"https://png.pngtree.com/png-vector/20240706/ourmid/pngtree-accessories-for-cats-cat-scratching-post-png-image_13011231.png", stock: 10 },
-  { id: 6, nombre: "Arnés reflectante", descripcion: "Seguridad nocturna con materiales reflectantes y cómodos.", precio: 45.00, imagen:"https://papopet.pe/wp-content/uploads/2024/08/416.webp", stock: 10 },
+  { id: 6, nombre: "Arnes reflectante", descripcion: "Seguridad nocturna con materiales reflectantes y cómodos.", precio: 45.00, imagen:"https://papopet.pe/wp-content/uploads/2024/08/416.webp", stock: 10 },
   { id: 7, nombre: "Shampoo para perros", descripcion: "Fórmula suave que cuida el pelaje y elimina los malos olores.", precio: 22.00, imagen: "https://petstorelima.pe/wp-content/uploads/2023/01/shampoo-frescan-piel-sensible-.png", stock: 10 },
   { id: 8, nombre: "Transportadora div tamaños", descripcion: "Caja transportadora ventilada y segura para viajes.", precio: 110.00, imagen: "https://pintospetshop.com/wp-content/uploads/2023/11/Savic-Transportador-Plomo-para-Mascotas-Trotter-1-640x640.png", stock: 10 },
   { id: 9, nombre: "ropa de gato + tallas", descripcion: "Ropa para que tu michi tenga el mejor de los looks.", precio: 50.00, imagen: "https://magavet.co/wp-content/uploads/2021/01/RPC-Gato-DuoDry-Rosa.png", stock: 10 },
   { id: 10, nombre: "ropa de perro + tallas", descripcion: "Lo mejor de lo mejor en ropa para tu amigo canino favorito.", precio: 60.00, imagen: "https://www.macropaparamascotas.com/wp-content/uploads/2025/05/ropa-perro-camisetas.webp", stock: 10 }
 ];
+
